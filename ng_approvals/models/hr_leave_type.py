c667520a9bf46fed3dc7a7d5e26aa0424e38343e
ucheumeevuruo <ucheumeevuruo@yahoo.com>
2021-09-06 15:35:33 +0100

    HR Approval

--- ng_approvals/models/hr_leave_type.py
+++ ng_approvals/models/hr_leave_type.py
@@ -5,5 +5,5 @@ from odoo.exceptions import ValidationError
 class HolidaysType(models.Model):
     _inherit = "hr.leave.type"
 
-    validation_type = fields.Selection(selection_add=[('triple', 'Team Leader/ COO and Time Off Officer')])
+    # validation_type = fields.Selection(selection_add=[('triple', 'Team Leader/ COO and Time Off Officer')])
 #     state = fields.Selection(selection_add=[('to_approve', 'To approve')])
